e2ad71eef48a47b209301f4b1343fdb6a0052392
andrew.kurilchik@gmail.com <andrew.kurilchik@gmail.com>
2021-06-09 11:04:55 +0300

    Add project files.

---
+++ HW01.Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HW01.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            do
+            {
+                number = UserInput();
+                switch (number)
+                {
+                    case 0:
+                        Console.WriteLine(1);
+                        break;
+                    case 1:
+                        Console.WriteLine('A');
+                        break;
+                    case 2:
+                        Console.WriteLine('I');
+                        break;
+                    default:
+                        Console.WriteLine("See you soon!");
+                        break;
+                }
+            } while (number != 3);
+        }
+
+        static int UserInput()
+        {
+            Console.WriteLine("Please enter a number between 0 and 3:");
+            return int.TryParse(Console.ReadLine(), out int number) && number >= 0 && number < 4 ? number : UserInput();
+        }
+    }
+}
---
+++ HW01.Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW01.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> x = new();
+            for (double i = 0; i <= 1; i += 0.1)
+            {
+                x.Add(Math.Sqrt(Math.Asin(i)) - 2);
+            }
+            foreach (var item in x)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in x)
+            {                
+                Console.WriteLine(Math.Round(Math.Sin(Math.Pow(item + 2, 2)), 2));
+            }
+        }
+    }
+}
