a4c4586b6882f92d7703473d9d2c73187a9eda20
andrew.kurilchik@gmail.com <andrew.kurilchik@gmail.com>
2021-09-09 23:01:19 +0300

    Added MVC

---
+++ DataBase/CarRegistration/CarRegistration.WebApplication.Presentation/Controllers/DeveloperController.cs
@@ -0,0 +1,44 @@
+﻿using CarRegistration.BusinessLogicLayer.Services;
+using CarRegistration.WebApplication.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRegistration.WebApplication.Presentation.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        private DeveloperService _developerService;
+
+        public DeveloperController()
+        {
+            _developerService = new DeveloperService();
+        }
+
+        public IActionResult Index()
+        {
+            var developers = _developerService.GetList();
+
+            DevelopersViewModel developersView = new DevelopersViewModel() { Developers = new List<DeveloperViewModel>() };
+
+            foreach (var item in developers)
+            {
+                DeveloperViewModel developer = new DeveloperViewModel() { Age = item.Age, Name = item.Name };
+
+                developersView.Developers.Add(developer);
+            }
+
+            return View(developersView);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromForm]AddDeveloperDTO addDeveloperDTO)
+        {
+            _developerService.Add(addDeveloperDTO.Name, addDeveloperDTO.Age);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DataBase/CarRegistration/CarRegistration.WebApplication.Presentation/Models/DevelopersViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRegistration.WebApplication.Presentation.Models
+{
+    public class DevelopersViewModel
+    {
+        public List<DeveloperViewModel> Developers { get; set; }
+    }
+}
