1ce7bae4cd6d403fdf9229220afc89169919cb95
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2021-09-09 16:17:42 +0300

    Added homework MVC get post

---
+++ Web/WebApplicationMVC/WebApplicationMVC/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Controllers
+{
+
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var user = new UserViewModel() { Name = "Andrew", Age = 31 };
+            return View(user);
+        }
+
+        public IActionResult New(string name, string age)
+        {
+            var user = new UserViewModel() { Name = name, Age = int.Parse(age) };
+            return View(user);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
