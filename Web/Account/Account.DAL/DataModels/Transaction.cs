31cc74f546a70841e070e10d539e8a82a7b1f117
Andrew Kurilchik <andrew.kurilchik@gmail.com>
2021-09-16 16:27:43 +0300

    Added DbContext

---
+++ Web/Account/Account.DAL/DataModels/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Account.DAL.DataModels
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Web/Account/Account.DAL/DataModels/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Account.DAL.DataModels
+{
+    public class User
+    {
+        public User()
+        {
+            Transactions = new HashSet<Transaction>();
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<Transaction> Transactions { get; set; }
+    }
+}
